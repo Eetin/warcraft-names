f322285a8ee4c4d14f6aefc3afe748a8f66b95a6
Dmitry Lebedev <p17dmitry@gmail.com>
2016-08-10 12:17:40 +0000

    style(index): add semicolons

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import uniqueRandomArray from 'unique-random-array'
-const warcraftNames = require('./warcraft-names.json')
+import uniqueRandomArray from 'unique-random-array';
+const warcraftNames = require('./warcraft-names.json');
 
 const mainExport = {
   all: warcraftNames,
   random: uniqueRandomArray(warcraftNames),
-}
+};
 
-export default mainExport
-module.exports = mainExport // for CommonJS compatibility
+export default mainExport;
+module.exports = mainExport; // for CommonJS compatibility
