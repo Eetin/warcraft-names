922423b72469c5645176417bc9e87cda6eb54001
Dmitry Lebedev <p17dmitry@gmail.com>
2016-08-10 12:25:25 +0000

    fix(tests): add semicolons to test
    Not actually a fix, just to force npm publish.

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,20 @@
-import {expect} from 'chai'
-import warcraftNames from '.'
+import {expect} from 'chai';
+import warcraftNames from '.';
 
 describe('warcraft-names', () => {
   it('should have a list of all available names', () => {
-    expect(warcraftNames.all).to.satisfy(isArrayOfStrings)
-  })
+    expect(warcraftNames.all).to.satisfy(isArrayOfStrings);
+  });
 
   it('should allow me to get a random name from the list', () => {
-    expect(warcraftNames.random()).to.satisfy(isIncludedIn(warcraftNames.all))
-  })
-})
+    expect(warcraftNames.random()).to.satisfy(isIncludedIn(warcraftNames.all));
+  });
+});
 
 function isArrayOfStrings(array) {
-  return array.every(i => typeof i === 'string')
+  return array.every(i => typeof i === 'string');
 }
 
 function isIncludedIn(array) {
-  return item => array.includes(item)
+  return item => array.includes(item);
 }
